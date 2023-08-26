a3c002c4f3789945a0840b49c38852ecd0a19c09
preetpalsinghmehra <124759969+Preetpal1050@users.noreply.github.com>
2023-08-27 00:52:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://icanhazdadjoke.com/slack')
+fetch('AKfycbwpokVi6ROxcrFBX79Vr1Ps4DYb9i1jh96liBe4PQD4LHThAL35wpuxoWx8ImyL3Skh')
     .then(data => data.json())
     .then(Data => {
         const ID = Data.attachments[0].text;
